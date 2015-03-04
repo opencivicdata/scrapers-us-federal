dfd9f1660b319b853dc01a0ae5a2a473b50c7e84
crdunwel <clayton.dunwell@gmail.com>
2015-03-04 17:56:29 -0500

    prepare with unitedstate scraper in mind

--- unitedstates/__init__.py
+++ unitedstates/__init__.py
@@ -1,8 +1,13 @@
 from pupa.scrape import Jurisdiction, Organization
+
+# config file, see example file for how to configure
+from .config import Configuration
+
+# scrapers
 from .legislative import UnitedStatesLegislativeScraper
+from .bill import UnitedStatesBillScraper
 
-
-class UnitedStates(Jurisdiction):
+class UnitedStates(Jurisdiction, Configuration):
     classification = 'government'
     division_id = 'ocd-division/country:us'
 
@@ -17,6 +22,7 @@ class UnitedStates(Jurisdiction):
 
     scrapers = {
         "congress": UnitedStatesLegislativeScraper,
+        "bills": UnitedStatesBillScraper,
         # Executive Scraper here
     }
 
